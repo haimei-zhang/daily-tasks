8e4a264c9cef4671246cee636e6cb34a5f71587b
haimei-zhang <haimeizhang2015@gmail.com>
2021-06-30 02:32:01 +0100

    change default language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
 
     translateService.setTranslation('cn', CN);
     translateService.setTranslation('en', EN);
-    translateService.use('en');
+    translateService.use('cn');
   }
 
   changeLanguage(language): void {
