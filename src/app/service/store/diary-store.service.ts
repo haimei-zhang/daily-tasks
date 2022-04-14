7baa6d6a0bfd0f800d8cb90b3a564b3f3b306435
haimei-zhang <haimeizhang2015@gmail.com>
2022-04-14 21:38:56 +0100

    only show info under certain users

--- src/app/service/store/diary-store.service.ts
+++ src/app/service/store/diary-store.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import * as R from 'ramda';
 
 import { Habit } from '~models/habit.model';
-import { dateToTime } from '~utils/core.util';
+import { dateToTime, getLoggedInUser } from '~utils/core.util';
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +16,14 @@ export class DiaryStoreService {
   readonly currentAnnouncementSource = new BehaviorSubject<any>(this.currentAnnouncement);
   currentAnnouncement$ = this.currentAnnouncementSource.asObservable();
 
-  selfCareTasks$ = this.getObservable(this.angularFirestore.collection('self_care_tasks')) as Observable<Habit[]>;
-  sportsTasks$ = this.getObservable(this.angularFirestore.collection('sports_tasks')) as Observable<Habit[]>;
-  studyTasks$ = this.getObservable(this.angularFirestore.collection('study_tasks')) as Observable<Habit[]>;
-  meditationTasks$ = this.getObservable(this.angularFirestore.collection('meditation_tasks')) as Observable<Habit[]>;
-  gameTasks$ = this.getObservable(this.angularFirestore.collection('game_tasks')) as Observable<Habit[]>;
-  challengingTasks$ = this.getObservable(this.angularFirestore.collection('challenging_tasks')) as Observable<Habit[]>;
-  loveTasks$ = this.getObservable(this.angularFirestore.collection('love_tasks')) as Observable<Habit[]>;
-  movieTasks$ = this.getObservable(this.angularFirestore.collection('movie_tasks')) as Observable<Habit[]>;
+  selfCareTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('self_care_tasks')) as Observable<Habit[]>;
+  sportsTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('sports_tasks')) as Observable<Habit[]>;
+  studyTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('study_tasks')) as Observable<Habit[]>;
+  meditationTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('meditation_tasks')) as Observable<Habit[]>;
+  gameTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('game_tasks')) as Observable<Habit[]>;
+  challengingTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('challenging_tasks')) as Observable<Habit[]>;
+  loveTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('love_tasks')) as Observable<Habit[]>;
+  movieTasks$ = this.getObservable(this.getCurrentUserDataCollection().collection('movie_tasks')) as Observable<Habit[]>;
 
   constructor(private angularFirestore: AngularFirestore) {
   }
@@ -34,6 +34,10 @@ export class DiaryStoreService {
       subject.next(val);
     });
     return subject;
+  }
+
+  getCurrentUserDataCollection(): AngularFirestoreDocument {
+    return this.angularFirestore.collection('users').doc(getLoggedInUser().uid).collection('personal_data').doc('1');
   }
 
   updateCurrentAnnouncement(announcement): void {
@@ -48,16 +52,16 @@ export class DiaryStoreService {
 
   createHabit(dbName: string, habit: Habit): void {
     habit.completedDate = dateToTime(habit.completedDate);
-    this.angularFirestore.collection(dbName).add(habit);
+    this.getCurrentUserDataCollection().collection(dbName).add(habit);
   }
 
   deleteHabit(dbName: string, habit: Habit): void {
-    this.angularFirestore.collection(dbName).doc(habit.id).delete();
+    this.getCurrentUserDataCollection().collection(dbName).doc(habit.id).delete();
   }
 
   updateHabit(dbName: string, habit: Habit): void {
     habit.completedDate = dateToTime(habit.completedDate);
-    this.angularFirestore.collection(dbName).doc(habit.id).update(habit);
+    this.getCurrentUserDataCollection().collection(dbName).doc(habit.id).update(habit);
   }
 
 }
